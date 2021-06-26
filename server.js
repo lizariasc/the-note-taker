4a3140c17b3a57b3bf7ad374b92173e1aac242fc
LAgrl <lizethariasc14@gmail.com>
2021-06-26 13:44:37 -0700

    route to front end

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({extend: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// make files readily available 
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3001;
+const { static } = require("express");
 
 
 
