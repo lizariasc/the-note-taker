f500f923f29a12355ea244e3edb0b7fefcad2dd3
LAgrl <lizethariasc14@gmail.com>
2021-06-26 13:52:44 -0700

    get routes

--- server.js
+++ server.js
@@ -16,14 +16,27 @@ const PORT = process.env.PORT || 3001;
 const { static } = require("express");
 
 
+// get routes
 
-
-
-
-
+// return index.html file
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+// return notes.html file
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+// return all saved notes as JSON
+app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", (err, data) => {
+        if (err) throw err;
+        const parseData = JSON.parse(data)
+        return res.json(parseData);
+    });
+});
 
 
 
 app.listen(3000, () => {
-    console.log(`API server now on port 3000!`);
-  });
+       console.log(`API server now on port 3000!`);
+ });
